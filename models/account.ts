8dd6d3d0adea1efbea3cca34e5c7e6bbfcd7af5e
飞小RAN <55832499+xiaofeiTM233@users.noreply.github.com>
2025-09-20 14:34:20 +0800

    feat: account

---
+++ models/account.ts
@@ -0,0 +1,16 @@
+// models/account.ts
+import mongoose from 'mongoose';
+
+const accountSchema = new mongoose.Schema({
+  platform: { type: String, required: true },
+  aid: { type: String, required: true, unique: true },
+  uid: { type: String, required: true },
+  cookies: { type: Object, required: true },
+}, {
+  timestamps: true,
+  versionKey: false
+});
+
+const Account = mongoose.models.Account || mongoose.model('Account', accountSchema);
+
+export default Account;
