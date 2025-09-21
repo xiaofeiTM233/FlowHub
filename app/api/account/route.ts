13b6ed09704af26f5ce4633d1194fb10be07fb43
飞小RAN <55832499+xiaofeiTM233@users.noreply.github.com>
2025-09-21 16:04:10 +0800

    feat: aid stat

--- app/api/account/route.ts
+++ app/api/account/route.ts
@@ -8,9 +8,13 @@ import { qzoneStat } from '@/lib/adapter/qzone';
  * 获取并更新所有平台账号的stat
  * @returns 平台账号数组
  */
-export async function GET() {
+export async function GET(request: Request) {
   await dbConnect();
-  const accounts = await Account.find();
+  const aids = (() => {
+    const param = new URL(request.url).searchParams.get('platform');
+    return param ? param.split(',').map(s => s.trim()).filter(Boolean) : null;
+  })();
+  const accounts = aids ? await Account.find({ aid: { $in: aids } }) : await Account.find();
   const stats: { [key: string]: any } = {};
   for (const account of accounts) {
     let stat;
@@ -23,7 +27,7 @@ export async function GET() {
     await account.save();
     stats[account.aid] = stat;
   }
-  return Response.json({ code: 0, message: '已获取并更新所有账号的统计信息', data: stats }, { status: 200 });
+  return Response.json({ code: 0, message: '已获取并更新账号统计信息', data: stats }, { status: 200 });
 }
 
 /**
