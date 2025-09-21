0cf34b168f2e8de8123822d05f925869ca670176
飞小RAN <55832499+xiaofeiTM233@users.noreply.github.com>
2025-09-21 22:16:53 +0800

    refactor: 将account分为两个api

---
+++ app/api/account/create/route.ts
@@ -0,0 +1,25 @@
+// app/api/account/create/route.ts
+import dbConnect from '@/lib/db';
+import Account from '@/models/account';
+
+/**
+ * 创建或更新平台账号
+ * @param request 包含 platform, aid, uid, cookies 字段的请求体
+ * @returns 操作结果及平台账号对象
+ */
+export async function POST(request: Request) {
+  await dbConnect();
+  const { platform, aid, uid, cookies } = await request.json();
+
+  const theAccount = await Account.findOneAndUpdate(
+    { aid },
+    { platform, aid, uid, cookies },
+    {
+      new: true,
+      upsert: true,
+      setDefaultsOnInsert: true
+    }
+  );
+
+  return Response.json({ code: 0, message: '平台账号已更新', account: theAccount }, { status: 200 });
+}
--- app/api/account/stats/route.ts
+++ app/api/account/stats/route.ts
@@ -1,12 +1,12 @@
-// app/api/account/route.ts
+// app/api/account/stats/route.ts
 import dbConnect from '@/lib/db';
 import Account from '@/models/account';
 import { biliStat } from '@/lib/adapter/bili';
 import { qzoneStat } from '@/lib/adapter/qzone';
 
 /**
- * 获取并更新所有平台账号的stat
- * @returns 平台账号数组
+ * 获取并更新所有平台账号的统计信息
+ * @returns 平台账号的统计信息
  */
 export async function GET(request: Request) {
   await dbConnect();
@@ -28,26 +28,4 @@ export async function GET(request: Request) {
     stats[account.aid] = stat;
   }
   return Response.json({ code: 0, message: '已获取并更新账号统计信息', data: stats }, { status: 200 });
-}
-
-/**
- * 更新平台账号
- * @param request 包含 platform, aid, uid, cookies 字段的请求体
- * @returns 操作结果及平台账号对象
- */
-export async function POST(request: Request) {
-  await dbConnect();
-  const { platform, aid, uid, cookies } = await request.json();
-
-  const theAccount = await Account.findOneAndUpdate(
-    { aid },
-    { platform, aid, uid, cookies },
-    {
-      new: true,
-      upsert: true,
-      setDefaultsOnInsert: true
-    }
-  );
-
-  return Response.json({ code: 0, message: '平台账号已更新', account: theAccount }, { status: 200 });
 }
